bcfa372c2757f7369a1a240ec7f8f8713dac5e83
Timothy Harley <timothy.d.harley@gmail.com>
2018-10-24 20:41:01 -0500

    wip

--- javascripts/events.js
+++ javascripts/events.js
@@ -2,13 +2,24 @@ const searchbar = () => {
     $('#searchbar').keyup(() => {
         let search = $('#searchbar').val().toLowerCase();
         $('.searchables').filter(function() {
-            $(this).closest('.card').toggle($(this).text().toLowerCase().indexOf(search) > -1)
+            $(this).closest('.locationCard').toggle($(this).text().toLowerCase().indexOf(search) > -1)
        });
     })
 }
 
 const buttons = () => {
-    
+    $('#morning').click(() => {
+        $('.locationcard').not('.Morning').toggle();
+    })
+    $('#afternoon').click(() => {
+        $('.locationcard').not('.Afternoon').toggle();
+    })
+    $('#evening').click(() => {
+        $('.locationcard').not('.Evening').toggle();
+    })
+    $('#dark').click(() => {
+        $('.locationcard').not('.After').toggle();
+    })
 }
 
 export {searchbar, buttons}
