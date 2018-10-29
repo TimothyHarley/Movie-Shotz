22426fb1e7e562a5638332d1c9fbab2eb033aa4c
Timothy Harley <timothy.d.harley@gmail.com>
2018-10-29 17:21:01 -0500

    wip

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,12 +1,12 @@
 import {getMovies} from './data/movieData.js'
-import {getLocations} from './data/locationsData.js'
+import {initLocations} from './components/locationComponent.js'
 import {searchbar, buttons} from './events.js'
 
 console.log('js connected')
 
 const appInit = () => {
     getMovies();
-    getLocations();
+    initLocations();
     searchbar();
     buttons();
 };
